2d496c10e36fdfcf3f702b55ec346da91437f0ca
djordjeds <djordjedstanojevic@gmail.com>
2023-05-22 20:23:05 +0200

    Completed import and export of keys

--- src/gui/tabs/key_gen_tab.py
+++ src/gui/tabs/key_gen_tab.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import ttk, filedialog
 from src.gui.tabs.initial_login_tab import gui_start
 from src.pgp.consts.consts import KEY_SIZES, ASYMMETRIC_ENCRYPTION_ALGORITHMS, AlgorithmType, Algorithm
+from src.pgp.key.key import KeyPair
+from src.pgp.key.key_serializer import KeySerializer, conclude_pem_algorithm
+from src.pgp.key.manager import KeyManager
 from src.pgp.user.user import User
 
 
@@ -30,6 +33,47 @@ def generate_keypair_callback(user: User, generate_result_label: ttk.Label, emai
         generate_result_label.config(text=f"Error while generating keys: {e}")
 
 
+def import_keys_callback(user: User, import_keys_result_label: ttk.Label, path_private: str, path_public: str,
+                         email: str, password: str, key_type: str):
+    try:
+        key_manager = KeyManager(user.user_name)
+        key_type = AlgorithmType.ASYMMETRIC_ENCRYPTION if key_type == "Encryption" \
+            else AlgorithmType.SIGNING if key_type == "Signature" else None
+        key_manager.import_key_pair_from_pem(
+            public_key_pem_path=path_public,
+            private_key_pem_path=path_private,
+            email=email,
+            password=password,
+            algorithm_type=key_type
+        )
+        import_keys_result_label.config(text="Successfully loaded keys!")
+    except Exception as e:
+        print(f"Error while exporting message: {e}")
+        import_keys_result_label.config(text=f"Error while importing key: {e}")
+
+
+def export_keys_callback(user: User, export_keys_result_label: ttk.Label, path: str,
+                         password: str, email: str, key_type: str):
+    try:
+        algorithm_type = AlgorithmType.ASYMMETRIC_ENCRYPTION if key_type == "Encryption" \
+            else AlgorithmType.SIGNING if key_type == "Signature" else None
+        key_manager = KeyManager(user.user_name)
+        key_pair = key_manager.get_key_pair_by_user_mail(email, password, algorithm_type=algorithm_type)
+        key_serializer = KeySerializer()
+        key_serializer.export_private_key_to_pem(
+            key_pair=key_pair,
+            private_key_pem_path=path+"/private_key_"+user.user_name+"_"+key_type+".pem"
+        )
+        key_serializer.export_public_key_to_pem(
+            key_pair=key_pair,
+            public_key_pem_path=path+"/public_key_"+user.user_name+"_"+key_type+".pem"
+        )
+        export_keys_result_label.config(text="Successfully exported public and private keys!")
+    except Exception as e:
+        print(f"Error while exporting keys: {e}")
+        export_keys_result_label.config(text=f"Error while exporting keys: {e}")
+
+
 def keyg_tab_gen(notebook, user, logout_callback):
     def update_signature_label(asym_algo_combo, signature_label):
         selected_asym_algo = asym_algo_combo.get()
@@ -38,21 +82,35 @@ def keyg_tab_gen(notebook, user, logout_callback):
         elif selected_asym_algo == 'ElGamal':
             signature_label.config(text="DSA Signature")
 
-    def browse_private_key():
+    def browse_import_private_key():
         file_path = filedialog.askopenfilename(filetypes=[("Key Files", "*.pem")])
-        private_key_entry.delete(0, tk.END)
-        private_key_entry.insert(tk.END, file_path)
+        import_private_key_entry.delete(0, tk.END)
+        if file_path:
+            import_private_key_entry.insert(tk.END, file_path)
+            import_keys_result_label.config(text="")
+        else:
+            import_keys_result_label.config(text="No .pem file selected.")
 
-    def browse_public_key():
+    def browse_import_public_key():
         file_path = filedialog.askopenfilename(filetypes=[("Key Files", "*.pem")])
-        public_key_entry.delete(0, tk.END)
-        public_key_entry.insert(tk.END, file_path)
+        import_public_key_entry.delete(0, tk.END)
+        if file_path:
+            import_public_key_entry.insert(tk.END, file_path)
+            import_keys_result_label.config(text="")
+        else:
+            import_keys_result_label.config(text="No .pem file selected.")
+
+    def browse_export_key():
+        directory = filedialog.askdirectory()
+        export_key_entry.delete(0, tk.END)
+        if directory:
+            export_key_entry.insert(tk.END, directory)
+            export_keys_result_label.config(text="")
+        else:
+            export_keys_result_label.config(text="No export folder selected.")
 
     def generate_keys_callback():
         generate_result_label.config(text="Keys generated successfully!")
-
-    def import_keys_callback():
-        import_keys_result_label.config(text="Keys imported successfully!")
 
     key_gen_tab = ttk.Frame(notebook)
     notebook.add(key_gen_tab, text="Key Generation")
@@ -108,9 +166,13 @@ def keyg_tab_gen(notebook, user, logout_callback):
     password_entry.config(width=23)
     ttk.Label(key_gen_tab).grid(row=7)
 
+    # Generate result label
+    generate_result_label = ttk.Label(key_gen_tab, text="")
+    generate_result_label.grid(row=8, column=0, columnspan=2)
+
     # Generate Keys button
     generate_btn = ttk.Button(key_gen_tab, text="Generate Keys")
-    generate_btn.grid(row=8, column=0, columnspan=2, padx=10, pady=10)
+    generate_btn.grid(row=9, column=0, columnspan=2, padx=10, pady=10)
     generate_btn.bind(
         "<Button-1>", lambda event: generate_keypair_callback(
             user,
@@ -123,48 +185,156 @@ def keyg_tab_gen(notebook, user, logout_callback):
         )
     )
 
-    # Generate result label
-    generate_result_label = ttk.Label(key_gen_tab, text="")
-    generate_result_label.grid(row=9, column=0, columnspan=2)
+    # Separator
+    separator1 = ttk.Separator(key_gen_tab, orient="horizontal")
+    separator1.grid(row=10, column=0, columnspan=3, padx=10, pady=10, sticky="we")
 
     # Import keys label
     import_keys_label = ttk.Label(key_gen_tab, text="Import keys:")
-    import_keys_label.grid(row=10, column=0, columnspan=2, padx=10, pady=10)
+    import_keys_label.grid(row=11, column=0, columnspan=2, padx=10, pady=10)
 
-    # Private Key
-    private_key_label = ttk.Label(key_gen_tab, text="Private Key:")
-    private_key_label.grid(row=11, column=0, padx=12, pady=4, sticky=tk.W)
-    private_key_entry = ttk.Entry(key_gen_tab)
-    private_key_entry.grid(row=11, column=1, padx=12, pady=4)
-    private_key_entry.config(width=23)
+    # Key Entry for Import Public Key
+    import_public_key_label = ttk.Label(key_gen_tab, text="Public key:")
+    import_public_key_label.grid(row=12, column=0, padx=12, pady=4, sticky=tk.W)
+    import_public_key_entry = ttk.Entry(key_gen_tab)
+    import_public_key_entry.grid(row=12, column=1, padx=12, pady=4)
+    import_public_key_entry.config(width=23)
 
-    # Browse button
-    browse_private_key_button = ttk.Button(key_gen_tab, text="Browse", command=browse_private_key)
-    browse_private_key_button.grid(row=11, column=2, padx=4, pady=4)
+    # Browse button for Import Public Key
+    browse_import_public_key_button = ttk.Button(key_gen_tab, text="Browse", command=browse_import_public_key)
+    browse_import_public_key_button.grid(row=12, column=2, padx=4, pady=4)
 
-    # Public Key
-    public_key_label = ttk.Label(key_gen_tab, text="Public Key:")
-    public_key_label.grid(row=12, column=0, padx=12, pady=4, sticky=tk.W)
-    public_key_entry = ttk.Entry(key_gen_tab)
-    public_key_entry.grid(row=12, column=1, padx=12, pady=4)
-    public_key_entry.config(width=23)
+    # Key Entry for Import Private Key
+    import_private_key_label = ttk.Label(key_gen_tab, text="Private key:")
+    import_private_key_label.grid(row=13, column=0, padx=12, pady=4, sticky=tk.W)
+    import_private_key_entry = ttk.Entry(key_gen_tab)
+    import_private_key_entry.grid(row=13, column=1, padx=12, pady=4)
+    import_private_key_entry.config(width=23)
 
-    # Browse button
-    browse_public_key_button = ttk.Button(key_gen_tab, text="Browse", command=browse_public_key)
-    browse_public_key_button.grid(row=12, column=2, padx=4, pady=4)
+    # Browse button for Import Private Key
+    browse_import_private_key_button = ttk.Button(key_gen_tab, text="Browse", command=browse_import_private_key)
+    browse_import_private_key_button.grid(row=13, column=2, padx=4, pady=4)
 
-    # Import Keys button
-    import_keys_button = ttk.Button(key_gen_tab, text="Import", command=import_keys_callback)
-    import_keys_button.grid(row=13, column=0, columnspan=2, padx=10, pady=10)
+    # Email label for Import
+    import_keys_email_label = ttk.Label(key_gen_tab, text="Email:")
+    import_keys_email_label.grid(row=14, column=0, padx=12, pady=4, sticky=tk.W)
+
+    # Email entry for Import
+    import_keys_email_entry = ttk.Entry(key_gen_tab)
+    import_keys_email_entry.grid(row=14, column=1, padx=12, pady=4)
+    import_keys_email_entry.config(width=23)
+
+    # Password label for Import
+    import_keys_password_label = ttk.Label(key_gen_tab, text="Password:")
+    import_keys_password_label.grid(row=15, column=0, padx=12, pady=4, sticky=tk.W)
+
+    # Password entry for Import
+    import_keys_password_entry = ttk.Entry(key_gen_tab, show="*")
+    import_keys_password_entry.grid(row=15, column=1, padx=12, pady=4)
+    import_keys_password_entry.config(width=23)
+
+    # Key type label for Import
+    import_key_type_label = ttk.Label(key_gen_tab, text="Key type:")
+    import_key_type_label.grid(row=16, column=0, padx=12, pady=4, sticky=tk.W)
+
+    # Key type combobox for Import
+    import_key_type_combo = ttk.Combobox(key_gen_tab, values=["Encryption", "Signature"], state="readonly")
+    import_key_type_combo.grid(row=16, column=1, padx=12, pady=4)
+    import_key_type_combo.config(width=21)
+    import_key_type_combo.set("Encryption")
 
     # Import keys result label
     import_keys_result_label = ttk.Label(key_gen_tab, text="")
-    import_keys_result_label.grid(row=14, column=0, columnspan=2)
+    import_keys_result_label.grid(row=17, column=0, columnspan=2)
+
+    # Import Keys button
+    import_keys_button = ttk.Button(key_gen_tab, text="Import")
+    import_keys_button.grid(row=18, column=0, columnspan=2, padx=10, pady=10)
+    import_keys_button.bind(
+        "<Button-1>", lambda event: import_keys_callback(
+            user,
+            import_keys_result_label,
+            path_public=import_public_key_entry.get(),
+            path_private=import_private_key_entry.get(),
+            email=import_keys_email_entry.get(),
+            password=import_keys_password_entry.get(),
+            key_type=import_key_type_combo.get()
+        )
+    )
+
+    # Separator
+    separator2 = ttk.Separator(key_gen_tab, orient="horizontal")
+    separator2.grid(row=19, column=0, columnspan=3, padx=10, pady=10, sticky="we")
+
+    # Export keys label
+    export_keys_label = ttk.Label(key_gen_tab, text="Export keys:")
+    export_keys_label.grid(row=20, column=0, columnspan=2, padx=10, pady=10)
+
+    # Key Entry for Export
+    export_key_label = ttk.Label(key_gen_tab, text="Export location:")
+    export_key_label.grid(row=21, column=0, padx=12, pady=4, sticky=tk.W)
+    export_key_entry = ttk.Entry(key_gen_tab)
+    export_key_entry.grid(row=21, column=1, padx=12, pady=4)
+    export_key_entry.config(width=23)
+
+    # Browse button for Export
+    browse_export_key_button = ttk.Button(key_gen_tab, text="Browse", command=browse_export_key)
+    browse_export_key_button.grid(row=21, column=2, padx=4, pady=4)
+
+    # Password label for Export
+    export_password_label = ttk.Label(key_gen_tab, text="Password:")
+    export_password_label.grid(row=22, column=0, padx=12, pady=4, sticky=tk.W)
+
+    # Password entry for Export
+    export_password_entry = ttk.Entry(key_gen_tab, show="*")
+    export_password_entry.grid(row=22, column=1, padx=12, pady=4)
+    export_password_entry.config(width=23)
+
+    # Key selection label for Export
+    export_email_selection_label = ttk.Label(key_gen_tab, text="Email:")
+    export_email_selection_label.grid(row=23, column=0, padx=12, pady=4, sticky=tk.W)
+
+    # Key selection combobox for Export
+    export_email_selection_combo = ttk.Combobox(
+        key_gen_tab,
+        values=list(user.key_manager.get_private_keyring_dictionary().keys()),
+        state="readonly"
+    )
+    export_email_selection_combo.grid(row=23, column=1, padx=12, pady=4)
+    export_email_selection_combo.config(width=21)
+
+    # Key type label for Export
+    export_key_type_label = ttk.Label(key_gen_tab, text="Key type:")
+    export_key_type_label.grid(row=24, column=0, padx=12, pady=4, sticky=tk.W)
+
+    # Key type combobox for Export
+    export_key_type_combo = ttk.Combobox(key_gen_tab, values=["Encryption", "Signature"], state="readonly")
+    export_key_type_combo.grid(row=24, column=1, padx=12, pady=4)
+    export_key_type_combo.config(width=21)
+    export_key_type_combo.set("Encryption")
+
+    # Export keys result label
+    export_keys_result_label = ttk.Label(key_gen_tab, text="")
+    export_keys_result_label.grid(row=25, column=0, columnspan=2)
+
+    # Export Keys button
+    export_keys_button = ttk.Button(key_gen_tab, text="Export")
+    export_keys_button.grid(row=26, column=0, columnspan=2, padx=10, pady=10)
+    export_keys_button.bind(
+        "<Button-1>", lambda event: export_keys_callback(
+            user,
+            export_keys_result_label,
+            path=export_key_entry.get(),
+            password=export_password_entry.get(),
+            email=export_email_selection_combo.get(),
+            key_type=export_key_type_combo.get()
+        )
+    )
 
     # Logout
     logout_separator = ttk.Separator(key_gen_tab, orient="horizontal")
-    logout_separator.grid(row=15, column=0, columnspan=12, padx=0, pady=10, sticky="we")
+    logout_separator.grid(row=27, column=0, columnspan=3, padx=10, pady=10, sticky="we")
     username_label = ttk.Label(key_gen_tab, text=f"Current user: {user.user_name}")
-    username_label.grid(row=16, column=0, padx=12, pady=4, sticky=tk.W)
+    username_label.grid(row=28, column=0, padx=12, pady=4, sticky=tk.W)
     logout_btn = ttk.Button(key_gen_tab, text="Logout", command=logout_callback)
-    logout_btn.grid(row=17, column=0, padx=12, pady=4, sticky=tk.W)
+    logout_btn.grid(row=29, column=0, padx=12, pady=4, sticky=tk.W)
